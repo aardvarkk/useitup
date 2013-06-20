84052e0c6286e4d28b06435dfa03f03e9b53a47a
unknown <clarkson@CLARKSON.na.qualcomm.com>
2013-06-20 11:59:21 -0400

    Adding Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Useitup::Application.config.secret_token = '538e0d8a367f17099b49f7141a25b970ddbcaf0a6ccd4d2f2f519b12b27c125434bb9a530afe448ab3e9d931b04745ad5ab5d8243313af1371d463eda3ad2872'
