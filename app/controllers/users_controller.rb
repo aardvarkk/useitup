af221e5484d1ff4d2dbd9baf1f071b0d948a591e
aardvarkk <clarksonian@gmail.com>
2013-06-21 14:39:23 -0400

    Cleanup -- includes maybe isn't required? Maybe just does joins afterward though, so includes might be more effective.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def show
-    @available = current_user.available_ingredients.includes(:ingredient)
-    @pantry = current_user.pantry_ingredients.includes(:ingredient)
+    @available = current_user.available_ingredients
+    @pantry = current_user.pantry_ingredients
   end
 
 end
