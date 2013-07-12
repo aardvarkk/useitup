88a3b9840c9110a867f017eac7f1a1320a289487
aardvarkk <clarksonian@gmail.com>
2013-07-12 16:42:50 -0400

    Active ingredients showing options in order.

--- app/models/measure_type.rb
+++ app/models/measure_type.rb
@@ -1,4 +1,12 @@
 class MeasureType < ActiveRecord::Base
   has_many :measure_conversions
   attr_accessible :name
+
+  # Return measure types in alphabetical order, but with grams first
+  def self.in_order
+  	@measure_types = MeasureType.where('name != ?', 'Gram')
+  	@measure_types.sort_by!{ |m| m[:name] }
+  	@measure_types.unshift(MeasureType.where(name: 'Gram').first)
+  end
+
 end
